60d1ab4bdfb653234931e515dac96739a4c6ec8a
Lei Yan <lyan6@uthsc.edu>
2016-11-22 10:48:07 -0600

    Update deletetemp.sh

--- gn_deletetemp/deletetemp.sh
+++ gn_deletetemp/deletetemp.sh
@@ -1,6 +1,6 @@
 GNRoot="/gnshare/gn"
-find $GNRoot/web/tmp/	-mmin +100 -exec rm -rfv {} \;
-find $GNRoot/web/image/	-mmin +100 -exec rm -rfv {} \;
-mkdir $GNRoot/web/tmp
-mkdir $GNRoot/web/image
+find $GNRoot/web/tmp	-mmin +100 -exec rm -rfv {} \;
+find $GNRoot/web/image	-mmin +100 -exec rm -rfv {} \;
+mkdir -p $GNRoot/web/tmp
+mkdir -p $GNRoot/web/image
 chown -R apache:apache $GNRoot
