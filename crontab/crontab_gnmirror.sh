dc52735e43f7fed1af4fe088851a05009fc7bde8
root <root@lily.uthsc.edu>
2016-11-22 18:00:40 -0600

    Committer: root <root@lily.uthsc.edu>

--- crontab/crontab_gnmirror.sh
+++ crontab/crontab_gnmirror.sh
@@ -2,6 +2,6 @@ sh /home/leiyan/sysmaintenance/ntpdate/ntpdate.sh				1>/home/leiyan/sysmaintenan
 
 sh /home/leiyan/sysmaintenance/update_sysmaintenance/update_from_spring211.sh	1>/home/leiyan/sysmaintenance/update_sysmaintenance/update_from_spring211.log	2>&1
 
-sh /home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.sh			1>/home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.log			2>&1
 sh /home/leiyan/sysmaintenance/gn_update_db/gnupdatedb_from_spring211.sh	1>/home/leiyan/sysmaintenance/gn_update_db/gnupdatedb_from_spring211.log	2>&1
 sh /home/leiyan/sysmaintenance/gn_update_src/gnupdatesrc_from_spring211.sh	1>/home/leiyan/sysmaintenance/gn_update_src/gnupdatesrc_from_spring211.log	2>&1
+sh /home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.sh			1>/home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.log			2>&1
--- crontab/crontab_spring211.sh
+++ crontab/crontab_spring211.sh
@@ -1,5 +1,5 @@
 sh /home/leiyan/sysmaintenance/update_sysmaintenance/update_from_lily.sh	1>/home/leiyan/sysmaintenance/update_sysmaintenance/update_from_lily.log	2>&1
 
-sh /home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.sh			1>/home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.log			2>&1
 sh /home/leiyan/sysmaintenance/gn_update_db/gnupdatedb_from_lily.sh		1>/home/leiyan/sysmaintenance/gn_update_db/gnupdatedb_from_lily.log		2>&1
 sh /home/leiyan/sysmaintenance/gn_update_src/gnupdatesrc_from_lily.sh		1>/home/leiyan/sysmaintenance/gn_update_src/gnupdatesrc_from_lily.log		2>&1
+sh /home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.sh			1>/home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.log			2>&1
