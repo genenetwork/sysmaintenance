16fb8761a8ca30c08beeef6793d50ec52be93216
Lei Yan <lyan6@uthsc.edu>
2017-10-10 18:14:16 -0500

    fix crontab/crontab_gnmirror.sh

--- crontab/crontab_gnmirror.sh
+++ crontab/crontab_gnmirror.sh
@@ -1,7 +1,5 @@
-sh /home/leiyan/sysmaintenance/ntpdate/ntpdate.sh				1>/home/leiyan/sysmaintenance/ntpdate/ntpdate.log				2>&1
-
 sh /home/leiyan/sysmaintenance/update_sysmaintenance/update_from_spring211.sh	1>/home/leiyan/sysmaintenance/update_sysmaintenance/update_from_spring211.log	2>&1
-
+sh /home/leiyan/sysmaintenance/ntpdate/ntpdate.sh				1>/home/leiyan/sysmaintenance/ntpdate/ntpdate.log				2>&1
 sh /home/leiyan/sysmaintenance/gn_update_db/gnupdatedb_from_spring211.sh	1>/home/leiyan/sysmaintenance/gn_update_db/gnupdatedb_from_spring211.log	2>&1
 sh /home/leiyan/sysmaintenance/gn_update_src/gnupdatesrc_from_spring211.sh	1>/home/leiyan/sysmaintenance/gn_update_src/gnupdatesrc_from_spring211.log	2>&1
 sh /home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.sh			1>/home/leiyan/sysmaintenance/gn_deletetemp/deletetemp.log			2>&1
