c00533d30164eccc1e995a751dfb709f2816dc31
root <root@tyche.uthsc.edu>
2017-11-27 00:13:12 -0600

    update crontab/crontab_tyche.sh

--- crontab/crontab_tyche.sh
+++ crontab/crontab_tyche.sh
@@ -1,3 +1,2 @@
-sh /home/leiyan/sysmaintenance/ntpdate/ntpdate.sh		1>/home/leiyan/sysmaintenance/ntpdate/ntpdate.log		2>&1
 sh /home/leiyan/sysmaintenance/gn_backup/backup.sh		1>/home/leiyan/sysmaintenance/gn_backup/backup.log		2>&1
 sh /home/leiyan/sysmaintenance/gn_backup/backup_gnfiles.sh	1>/home/leiyan/sysmaintenance/gn_backup/backup_gnfiles.log	2>&1
