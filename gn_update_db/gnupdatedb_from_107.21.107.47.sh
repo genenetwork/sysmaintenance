65c04ada3d13f608bfea07f2235b249f88087185
root <root@lily.uthsc.edu>
2017-12-19 10:27:49 -0600

    update

--- gn_update_db/gnupdatedb_from_107.21.107.47.sh
+++ gn_update_db/gnupdatedb_from_107.21.107.47.sh
@@ -1,2 +1,4 @@
+/sbin/service mysqld stop
 rsync -avP --delete -e "ssh -i /home/leiyan/awskey/lyan6/leiyan.pem" root@107.21.107.47:/var/lib/mysql/db_webqtl/ /var/lib/mysql/db_webqtl/
 chown -R mysql:mysql /var/lib/mysql/db_webqtl
+/sbin/service mysqld start
